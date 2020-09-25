d5fa347594488239912c4798cfa0e9b29ac1daa6
citlalli sanchez <citlalli.sanchez2890@alumnos.udg.mx>
2020-09-25 14:08:33 -0500

    se creo la funcion para registrar 5 numeros

---
+++ introd.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+void mostrar(int n,char cadena[])
+{
+    for(size_t i=0;i<n;i++)
+    {
+        printf("%s",cadena);
+    }
+}
+void capturarEnteros()
+{
+    int enteros[5];
+    float promedio;
+    int suma=0;
+    for(size_t i =0;i<5;i++)
+    {
+        scanf("%i",&enteros[i]);
+    }
+    for(size_t i =0;i<5;i++)
+    {
+        printf("%i\n",enteros[i]);
+    }
+    for(size_t i =0;i<5;i++)
+    {
+        suma=suma+enteros[i];
+    }
+   printf("suma = %i\n",suma);
+   printf("promedio = %.2f\n",suma/5.0);
+
+}
+int main()
+{
+    int op;
+    char cadena[20];
+    int n;
+    do{
+        printf("1)capturar enteros\n");
+        printf("2)mostrar cadena n veces\n");
+        printf("3)agregar personaje\n");
+        printf("4)mostrar personaje\n");
+        printf("0)salir\n");
+        scanf("%i",&op);
+        fflush(stdin);
+        switch(op)
+        {
+            case 1:
+                capturarEnteros();
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+                break;
+
+        }
+
+    }while(op!=0);
+}
